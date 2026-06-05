f8ffe8c5418e60fd140e1132b83c964cf52595c8
Daniel Martin <danielmartindevelops@gmail.com>
2026-06-05 16:33:43 +0100

    Initial ASP.net commit

---
+++ Shift Planner - API/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Shift_Planner___API
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
