13a800c04fa8cc6593e77818fb4982a6b842e991
Daniel Martin <danielmartindevelops@gmail.com>
2026-06-07 07:43:09 +0100

    Added in Validation
    - Added in validation for data entry in Employee.cs and Shift.cs models

--- Shift Planner - API/Models/Employee.cs
+++ Shift Planner - API/Models/Employee.cs
@@ -1,11 +1,22 @@
-﻿namespace Shift_Planner___API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shift_Planner___API.Models
 {
     public class Employee
     {
+        [Required]
         public int EmployeeID { get; set; }
+
+        [Required(ErrorMessage = "Employee name is required")]
+        [StringLength(100,
+            ErrorMessage = "Name cannot exceed 100 characters")]
         public string Name { get; set; } = string.Empty;
         public DateTime StartDate {  get; set; }
+
+        [Range(1, 60)]
         public int WeeklyHours { get; set; }
+
+        [Required]
         public EmployeeRole.Role Role { get; set; }
         public ICollection<Shift> Shifts { get; set; } = new List<Shift>();
     }
--- Shift Planner - API/Models/Shift.cs
+++ Shift Planner - API/Models/Shift.cs
@@ -1,12 +1,18 @@
-﻿namespace Shift_Planner___API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shift_Planner___API.Models
 {
     public class Shift
     {
         public int ShiftID { get; set; }
+        [Required]
         public int EmployeeID { get; set; }
         public Employee? Employee { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
+
+        [Required]
+        [Range(0, 180)]
         public int BreakDuration { get; set; }
         public ShiftDay.DayOfWeek Day { get; set; }
     }
